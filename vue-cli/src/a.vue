15495cc08bc8ff23ae8f479e7ba913e755228e55
imivory <imivory38@gmail.com>
2020-11-13 09:39:00 +0900

    vue-cli 내용 업로드

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,31 @@
+<!--
+vu 쓰고 tab
+
+var appHeader ={
+    template: '<div>header</div>',
+    methods:{
+        addNum: function(){
+
+        }
+    }
+}
+-->
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // javascript
+     methods:{
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
